b46cfd851d6945813b93f456531b6b411c107937
Vinny Allegra <vinnyallegra@gmail.com>
2022-11-14 19:20:09 -0500

    Print Update

--- adns_lookup.c
+++ adns_lookup.c
@@ -54,7 +54,7 @@ tcp_lookup(int sk, const int qtype, const char * query ){
 
     msg.type = qtype;
     message_set_body(&msg, query);
-    mu_pr_debug("%s: request: id=%" PRIu32 ", type=%" PRIu16 ", body_len=%" PRIu16 ", answer=\"%s\"",
+    mu_pr_debug("%s: to_send: id=%" PRIu32 ", type=%" PRIu16 ", body_len=%" PRIu16 ", answer=\"%s\"",
         peer_str, msg.id, msg.type, msg.body_len, msg.body);
 
     n = message_serialize(&msg, buf, sizeof(buf));
